d6d491018d9b1728f146da3f445dacb2aa4b4aad
Rakieft <kieftraphter.joly1@gmail.com>
2025-09-18 11:57:11 -0400

    week 3 commit

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // config options here
-}
-
-module.exports = nextConfig
+import type { NextConfig } from 'next';
+ 
+const nextConfig: NextConfig = {
+  // experimental: {
+  //   ppr: 'incremental'
+  // }
+};
+ 
+export default nextConfig;
